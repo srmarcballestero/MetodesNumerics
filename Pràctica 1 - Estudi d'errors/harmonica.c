ff393a2f0eef35d4debb452fa4e808a08a010d45
srmarcballestero <marconiballestero@gmail.com>
2021-09-14 13:29:38 +0200

    Pràctica 1

---
+++ Pràctica 1 - Estudi d'errors/harmonica.c
@@ -0,0 +1,23 @@
+/* harmonica.c */
+/* calcula terme a terme el valor de les sumes parcials de la sèrie harmonica
+   i s'atura quan s_n-1 = s_n
+*/
+
+#include <stdio.h>
+
+
+int main(void) {
+  float sn = 0., sn1 = 1.;
+  int i;
+
+  for (i = 1; sn != sn1; ++i) {
+    sn1 = sn;
+    sn += 1./(float) i;
+
+    printf("sn = %.20f, i = %d\n", sn, i);
+  }
+
+  printf("Programa aturat després de %d iteracions.\n", i);
+
+  return 0;
+}
