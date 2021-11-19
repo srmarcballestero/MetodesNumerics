30f6e03446d74ac2a90ca19ebd8027424f467e9d
srmarcballestero <marconiballestero@gmail.com>
2021-11-19 14:39:48 +0100

    Pràctica 3

--- Pràctica 3 - Interpolació polinònica i aplicacions/interpolacio.c
+++ Pràctica 3 - Interpolació polinònica i aplicacions/interpolacio.c
@@ -104,3 +104,16 @@ void interpolacio(double *x, double *f, int n, double *c) {
 
   return;
 }
+
+
+/*
+  RESPOSTES A LES PREGUNTES
+    (3) Veure fitxers "runge.res" i "runge.png".
+    (4) Veure fitxers "txevixev.res" i "txevixev.res". En tots dos casos, els
+        polinomis calculats interpolen la funcio proposada en els punts
+        corresponents. Si mes no, observem que en el cas de l'interpolacio
+        en les abscisses de Txevixev, el polinomi s'aproxima millor a la corba
+        de la funcio proposada, ja que, en els punts allunyats del maxim local
+        de la funcio, el polinomi d'interpolacio no presenta tantes oscilacions
+        com el que s'obte interpolant abscisses lineals.
+*/
