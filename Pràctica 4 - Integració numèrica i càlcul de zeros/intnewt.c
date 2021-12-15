c09b50fc7ac61176495706976e1b714ebd78e501
srmarcballestero <marconiballestero@gmail.com>
2021-12-15 23:16:31 +0100

    Pràctica 4

--- Pràctica 4 - Integració numèrica i càlcul de zeros/intnewt.c
+++ Pràctica 4 - Integració numèrica i càlcul de zeros/intnewt.c
@@ -8,13 +8,27 @@
 
 
 int main(void) {
-  int imax = 30;
-  double a, z = 0., z1 = 0., fz = 0., tol = 1.e-12;
+  int j = 1, jmax, imax = 30;
+  double a, z, z1, fz = 0., tol;
+
+  printf("a = ");
+  scanf("%lf", &a);
+
+  printf("tol = ");
+  scanf("%lf", &tol);
+
+  printf("Maxim d'iterats:");
+  scanf("%d", &jmax);
 
   printf("Aproximacio inicial del zero:");
   scanf("%lf", &z);
 
   do {
+    if (j >= jmax) {
+      printf("No s'ha pogut trobar un zero amb la precisio demanada!\n");
+      return 1;
+    }
+
     z1 = z;
     if (!duptrap(0., z, tol, &imax, &fz)) {
       z -= fz / f(z);
@@ -24,9 +38,11 @@ int main(void) {
       printf("Error en la integracio!\n");
       return 1;
     }
+    ++j;
   } while (fabs(z-z1) >= tol || fabs(fz) >= tol);
 
-  printf("Zero trobat: %.6e\n", z);
+  printf("Zero trobat: %.12e\n", z);
+  printf("Iteracions del metode: %d\n", j);
 
   return 0;
 }
